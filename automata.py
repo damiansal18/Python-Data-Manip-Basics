2f40d01ddf80536da7b2a300fbacf6ad512a7d57
damiansal18 <damians1847@gmail.com>
2022-11-23 00:05:53 -0800

    Delete automata.py

--- automata.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-simpsons = pd.read_html('https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
-print(len(simpsons)) 
-print(simpsons[1])
-
-premLeague = pd.read_csv('https://www.football-data.co.uk/mmz4281/2223/E0.csv')
-print(premLeague)
